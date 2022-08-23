ad23a951f7ff9cd9a664678eebfedb055c625efa
Hastin10 <hastinmodi@gmail.com>
2022-08-23 11:50:36 -0700

    #125 Find whether the given string is a palindrome considering only alphanumeric characters

---
+++ Desktop/Interview_prep/DS_and_Algo/NeetCode_150/Two_Pointers/125_Valid_Palindrome.cpp
@@ -0,0 +1,31 @@
+/*
+Link to problem - https://leetcode.com/problems/valid-palindrome/
+Approach - Iterate through the string using 2 pointers - from start and end and then compare their values
+Time complexity - O(n)
+Space complexity - O(1)
+*/
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        
+        while (i < j) {
+            while (!isalnum(s[i]) && i < j) {
+                i++;
+            }
+            while (!isalnum(s[j]) && i < j) {
+                j--;
+            }
+            if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
